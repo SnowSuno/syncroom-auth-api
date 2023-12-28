13ae30c173fba5dc996b2571a9e01b7130f9f834
Soonho Kwon <percy3368@gmail.com>
2023-12-28 21:48:21 +0900

    :sparkles: Setup cors

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,13 @@ import { z } from "zod";
 import { zValidator } from "@hono/zod-validator";
 
 import { login } from "./auth";
+import { cors } from "hono/cors";
 
 const app = new Hono().basePath("/api");
 
+app.use("/api/*", cors({
+  origin: ["https://syncroom.link", "http://localhost"],
+}));
 
 app.post("/login", zValidator("json", z.object({
   username: z.string(),
